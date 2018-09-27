6a8820f7f9636526aa2d3c525766491adc058130
Aaron Chapin <achapin@goodgamestudios.com>
2018-09-28 00:01:42 +0200

    Forces are loaded, and characters can be added to the force list

--- builder.js
+++ builder.js
@@ -2,6 +2,14 @@ var upgrades;
 var bos;
 var survivors;
 var mutants;
+var characters;
+
+var forceSection;
+var addButton;
+var addSection;
+var capsSection;
+
+var totalCaps;
 
 function getUrl(url){
 	var req = new XMLHttpRequest();
@@ -30,7 +38,6 @@ function loadURL(url){
 function upgradesLoaded(json)
 {
 	upgrades = json;
-	alert("upgrades loaded successfully");
 	var bosLoadPromise = loadURL("data/brotherhood_of_steel.json");
 	bosLoadPromise.then(bosLoaded);
 	bosLoadPromise.catch(function(){alert("bos load failed");});
@@ -38,7 +45,6 @@ function upgradesLoaded(json)
 
 function bosLoaded(json){
 	bos = json;
-	alert("bos loaded successfully");
 	var survivorLoadPromise = loadURL("data/survivors.json");
 	survivorLoadPromise.then(survivorsLoaded);
 	survivorLoadPromise.catch(function(){alert("survivor load failed");});
@@ -46,7 +52,6 @@ function bosLoaded(json){
 
 function survivorsLoaded(json){
 	survivors = json;
-	alert("survivors loaded successfully");
 	var mutantLoadPromise = loadURL("data/super_mutants.json");
 	mutantLoadPromise.then(mutantsLoaded);
 	mutantLoadPromise.catch(function(){alert("mutants load failed");});
@@ -54,7 +59,101 @@ function survivorsLoaded(json){
 
 function mutantsLoaded(json){
 	mutants = json;
-	alert("all data loaded!");
+	initListeners();
+}
+
+function initListeners(){
+	document.getElementById("switch-bos").addEventListener("click", switchBos, true);
+	document.getElementById("switch-mutants").addEventListener("click", switchMutants, true);
+	document.getElementById("switch-survivors").addEventListener("click", switchSurvivors, true);
+
+	forceSection = document.getElementById("force");
+	addSection = document.getElementById("addSection");
+	addButton = document.getElementById("addButton");
+	capsSection = document.getElementById("caps");
+	addButton.addEventListener("click", openAddSection);
+
+	switchBos();
+}
+
+function switchBos() {
+	characters = bos;
+	clearForce();
+}
+
+function switchMutants() {
+	characters = mutants;
+	clearForce();
+}
+
+function switchSurvivors() {
+	characters = survivors;
+	clearForce();
+}
+
+function clearForce(){
+	forceSection.innerHTML = "";
+	addSection.innerHTML = "";
+	var list = document.createElement("ul");
+	characters.forEach(function(characterElement){
+		var para = document.createElement("li");
+		var node = document.createTextNode(characterElement.name);
+		para.addEventListener("click", function() { addCharacter(characterElement);});
+		para.appendChild(node);
+		list.appendChild(para);
+	});
+	addSection.appendChild(list);
+	var close = document.createElement("p");
+	var closeButton = document.createTextNode("X");
+	close.addEventListener("click", closeAddSection);
+	close.appendChild(closeButton);
+	addSection.appendChild(close);
+	closeAddSection();
+	totalCaps = 0;
+	updateCaps();
+}
+
+function closeAddSection(){
+	addSection.style.display = "none";
+	addButton.style.display = "block";
+}
+
+function openAddSection(){
+	addButton.style.display = "none";
+	addSection.style.display = "block";
+}
+
+function addCharacter(characterElement){
+	var charaSection = document.createElement("div");
+	var nameSection = document.createElement("h1");
+	var name = document.createTextNode(characterElement.name);
+	nameSection.appendChild(name);
+	charaSection.appendChild(nameSection);
+
+	var costSection = document.createElement("p");
+	var cost = document.createTextNode(characterElement.cost);
+	costSection.appendChild(cost);
+	charaSection.appendChild(costSection);
+
+	var close = document.createElement("p");
+	var closeButton = document.createTextNode("X");
+	close.addEventListener("click", function() 
+		{
+			forceSection.removeChild(charaSection);
+			totalCaps -= characterElement.cost; //TODO: include equipment
+			updateCaps();
+		}
+	);
+	close.appendChild(closeButton);
+	charaSection.appendChild(close);
+	forceSection.appendChild(charaSection);
+
+	totalCaps += characterElement.cost;
+	updateCaps();
+}
+
+function updateCaps(){
+	capsSection.innerHTML = totalCaps;
 }
 
 function initialize(){
