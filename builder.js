a6a7a5e6881ba629128fbf5013d2c9583685f894
Aaron Chapin <aaron.chapin@gmail.com>
2020-02-20 23:39:03 +0100

    Fixed missing images, list missing previews

--- builder.js
+++ builder.js
@@ -132,6 +132,9 @@ function localizationLoaded(json){
 			if(!loc.hasOwnProperty(upgrade.name)){
 				missingKeys += upgrade.name + ", ";
 			}
+			if(!upgrade.hasOwnProperty("preview")){
+				missingPreview += upgrade.name+",";
+			}
 		});
 	});
 
